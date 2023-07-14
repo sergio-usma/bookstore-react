216aa85d952c4a6a88c4b1389c297117ffe32890
sergio-usma <sergioandresusma@hotmail.com>
2023-07-14 11:44:17 -0500

    Add root and categories routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Root from './routes/root';
+import  Categories from './routes/categories';
 
 function App() {
 
   return (
     <>
-      <div>
-
-      </div>
+        <header>
+            <Header />
+        </header>
+        <main>
+            <Routes>
+                <Route path="/" element={<Root />} />
+                <Route path="/categories" element={<Categories />} />
+            </Routes>
+        </main>
     </>
   )
 }
