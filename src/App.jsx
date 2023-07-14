7ff4382bebf15487bbc63fd04f62e8344503db7a
sergio-usma <sergioandresusma@hotmail.com>
2023-07-14 12:27:13 -0500

    Edit App and config files

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Header from './components/Header';
-import Root from './routes/root';
-import  Categories from './routes/categories';
+import Home from './routes/home';
+import Categories from './routes/categories';
 
 function App() {
 
   return (
-    <>
-        <header>
-            <Header />
-        </header>
-        <main>
-            <Routes>
-                <Route path="/" element={<Root />} />
-                <Route path="/categories" element={<Categories />} />
-            </Routes>
-        </main>
-    </>
+      <BrowserRouter>
+          <Header />
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/categories" element={<Categories />} />
+          </Routes>
+      </BrowserRouter>
   )
 }
 
