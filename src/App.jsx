c03a094d08d9270b7cdccf272a038d0c74878864
sergio-usma <sergioandresusma@hotmail.com>
2023-07-14 12:59:46 -0500

    FIX stylelint error

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import './App.css'
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Header from './components/Header';
-import Home from './routes/home';
-import Categories from './routes/categories';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./routes/home";
+import Categories from "./routes/categories";
 
 function App() {
-
   return (
-      <BrowserRouter>
-          <Header />
-          <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/categories" element={<Categories />} />
-          </Routes>
-      </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
