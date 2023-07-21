7780edd9b326bcf8bae4b1fe5f4fe9b8b46fcaa9
sergio-usma <sergioandresusma@hotmail.com>
2023-07-21 12:03:45 -0500

    Setup Provider and booksSlice

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./routes/home";
 import Categories from "./routes/categories";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
+      <Provider store={store}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </Provider>
     </BrowserRouter>
   );
 }
