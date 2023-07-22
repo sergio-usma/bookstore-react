8328f781d1afb42e144914a7eabe28187180e91f
sergio-usma <sergioandresusma@hotmail.com>
2023-07-22 13:24:34 -0500

    Add styles to index and main

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import "./styles/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
