98c92678eb640adc5e8aa2f93b5c6e9d7598bf4f
sergio-usma <sergioandresusma@hotmail.com>
2023-07-22 13:21:05 -0500

    Add styles to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,25 @@
 import { Link } from "react-router-dom";
+import "../styles/Header.css";
 
 const Header = () => (
   <header>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Books</Link>
-        </li>
-        <li>
-          <Link to="/categories">Categories</Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="header__container">
+      <div className="navbar__container">
+        <h1 className="logo">Bookstore CMS</h1>
+        <nav>
+          <ul>
+            <li className="books">
+              <Link to="/">BOOKS</Link>
+            </li>
+            <li>
+              <Link to="/categories">CATEGORIES</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div className="profile__container"></div>
+    </div>
   </header>
 );
 
