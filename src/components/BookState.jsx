08013ec7b8383a58e6f0587e9aa88d5b8832532d
sergio-usma <sergioandresusma@hotmail.com>
2023-07-21 19:13:43 -0500

    Refactor BookState to connect API

--- src/components/BookState.jsx
+++ src/components/BookState.jsx
@@ -1,15 +1,47 @@
-import { useSelector } from "react-redux";
-import BookElement from "./BookElement.jsx";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Book from "./BookElement.jsx";
+import { getBooks } from "../redux/api/apiConnect.js";
 
 function BookState() {
-  const { arrBooks } = useSelector((state) => state.books);
+  const { books, isLoading, length, hasError, errorMessage } = useSelector(
+    (store) => store.books,
+  );
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [length]);
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
+  if (hasError) {
+    return (
+      <div>
+        Something went wrong:
+        {errorMessage}
+      </div>
+    );
+  }
+
   return (
-    <section className="">
-      {arrBooks.map((book) => (
-        <BookElement bookArr={book} key={book.item_id} />
-      ))}
-    </section>
+    <div>
+      <h1>Book List</h1>
+      {length === 0 ? (
+        <div>There's nothing here. Please add a new book!</div>
+      ) : (
+        books.map((book) => (
+          <Book
+            author={book.author}
+            category={book.category}
+            title={book.title}
+            key={book.item_id}
+            id={book.item_id}
+          />
+        ))
+      )}
+    </div>
   );
 }
-
 export default BookState;
