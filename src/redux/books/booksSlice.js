12dd89b95dd047795e697bfd995c121a7c067c5a
sergio-usma <sergioandresusma@hotmail.com>
2023-07-21 19:12:54 -0500

    Refactor redux slices/reducers

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,43 +1,75 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
+import { addBook, getBooks, deleteBook } from '../api/apiConnect';
 
+function destruct(key, [element]) {
+  return {
+    item_id: key,
+    title: element.title,
+    author: element.author,
+    category: element.category,
+  };
+}
+// Initial state:
 const initialState = {
-  arrBooks: [
-    {
-      item_id: "item1",
-      title: "The Great Gatsby",
-      author: "John Smith",
-      category: "Fiction",
-    },
-    {
-      item_id: "item2",
-      title: "Anna Karenina",
-      author: "Leo Tolstoy",
-      category: "Fiction",
-    },
-    {
-      item_id: "item3",
-      title: "The Selfish Gene",
-      author: "Richard Dawkins",
-      category: "Nonfiction",
-    },
-  ],
+  books: [],
+  length: 0,
+  hasError: false,
+  errorMessage: '',
+  isLoading: false,
 };
-
 const bookSlice = createSlice({
-  name: "Books",
+  name: 'books',
   initialState,
-  reducers: {
-    addBook: (state, action) => {
-      const newBook = action.payload;
-      state.arrBooks.push(newBook);
-    },
-    removeBook: (state, action) => {
-      state.arrBooks = state.arrBooks.filter(
-        (book) => book.item_id !== action.payload,
-      );
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getBooks.pending, (state) => {
+      state.isLoading = true;
+      state.hasError = false;
+      state.errorMessage = '';
+    }).addCase(getBooks.rejected, (state, action) => {
+      state.isLoading = false;
+      state.hasError = true;
+      state.errorMessage = action.payload;
+    }).addCase(getBooks.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.hasError = false;
+      state.errorMessage = '';
+
+      const keys = Object.keys(action.payload);
+      state.books = keys.map((key) => destruct(key, action.payload[key]));
+      state.length = state.books.length;
+    }).addCase(deleteBook.pending, (state) => {
+      state.isLoading = true;
+      state.hasError = false;
+      state.errorMessage = '';
+    })
+        .addCase(deleteBook.fulfilled, (state) => {
+          state.isLoading = false;
+          state.hasError = false;
+          state.length -= 1;
+          state.errorMessage = '';
+        })
+        .addCase(deleteBook.rejected, (state, action) => {
+          state.hasError = true;
+          state.errorMessage = action.payload;
+        })
+        .addCase(addBook.pending, (state) => {
+          state.isLoading = true;
+          state.errorMessage = '';
+          state.hasError = false;
+        })
+        .addCase(addBook.fulfilled, (state) => {
+          state.isLoading = false;
+          state.length += 1;
+          state.hasError = false;
+        })
+        .addCase(addBook.rejected, (state, action) => {
+          state.isLoading = false;
+          state.hasError = true;
+          state.errorMessage = action.payload;
+        });
   },
 });
 
-export const { addBook, removeBook } = bookSlice.actions;
 export default bookSlice.reducer;
+
