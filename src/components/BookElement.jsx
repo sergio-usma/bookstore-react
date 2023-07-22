2e0ab9b847df8bf0957e05a0bc8a9900a0dbaefb
sergio-usma <sergioandresusma@hotmail.com>
2023-07-22 18:25:11 -0500

    Add styles to the BookElement component

--- src/components/BookElement.jsx
+++ src/components/BookElement.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import { useDispatch } from "react-redux";
 import { deleteBook } from "../redux/api/apiConnect.js";
 import "../styles/BookElement.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Book({ id, category, title, author }) {
   const dispatch = useDispatch();
@@ -26,6 +27,22 @@ function Book({ id, category, title, author }) {
             Edit
           </button>
         </div>
+      </div>
+
+      <div className="col-auto ms-auto border-end border-gray pe-5">
+        <div>Progress</div>
+        <div>
+          <h3 className="mb-0 h2 fw-bold">33%</h3>
+          <p className="text-muted mt-0">Completed</p>
+        </div>
+      </div>
+
+      <div className="col-auto ps-5 ">
+        <p className="text-muted m-0">CURRENT CHAPTER</p>
+        <p className="fw-bold">Chapter 7</p>
+        <button type="button" className="btn btn-outline-primary w-100">
+          Update progress
+        </button>
       </div>
     </div>
   );
