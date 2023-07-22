6f04d095cc158db1fa56a873ddd18ff87c1963f4
sergio-usma <sergioandresusma@hotmail.com>
2023-07-21 19:20:50 -0500

    Update APP to connect Bookstore API

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./routes/home";
 import Categories from "./routes/categories";
-import { Provider } from "react-redux";
-import store from "./redux/store.js";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Provider store={store}>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/categories" element={<Categories />} />
-        </Routes>
-      </Provider>
-    </BrowserRouter>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </>
   );
 }
 
